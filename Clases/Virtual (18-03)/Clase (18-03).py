e664e73da68f7c90a1a8c2eef5961de6abe271bd
Catalina <catagonzalezaraujo@gmail.com>
2026-03-20 09:58:53 -0300

    Arreglos de la TS1

---
+++ Clases/Virtual (18-03)/Clase (18-03).py
@@ -0,0 +1,250 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fs = 1000 #Hz, frecuencia de muestreo
+f0 = 1 #Hz, frecuencia de la senoidal
+n = 1000 #muestras por ciclo
+vmax = 1 #volts
+dc = 0 #valor medio, volts
+ts = 1/fs
+
+def mi_senoidal(vmax, dc, f0, n, fs, ph=0):
+    
+    tt = np.linspace(0, (n-1)*ts, n)
+    xx = dc + vmax*np.sin(2*np.pi*f0*tt+ph)
+    
+    return tt, xx
+
+tt, xx = mi_senoidal(vmax, dc, f0, n, fs, ph=0)
+plt.figure()
+plt.plot(tt, xx, label = 'Senoidal original')
+plt.legend()
+
+#%% Señal con ruido
+#Agregamos una señal aleatoria (ruido) a la señal pura
+
+sigma = 1
+mu = 0
+SNR = 20
+Pr = 10**(-SNR/10)
+U_n = np.random.normal(mu,np.sqrt(Pr),n)
+# U_n = np.random.normal(mu,sigma,n)
+
+xxn = xx + U_n
+
+plt.xlabel('Tiempo [segundos]')
+plt.ylabel('Amplitud [V]')
+plt.plot(tt, xxn, label ='Senoidal con Ruido')
+plt.grid()
+plt.legend()
+plt.show()
+
+#%% Potencia de señal
+#Fijamos en 1 Watt el valor de potencia de la señal para despejar Pr
+#Queda SNR = 10* Log(1/Pr) y con eso regulo cuanto necesito de Pr para el
+#SNR que busco
+
+vmax = np.sqrt(2)
+tt, xx = mi_senoidal(vmax, dc, f0, n, fs, ph=0)
+Px = np.var(xx)
+SNR = 10
+Pr = 10**(SNR/10)
+
+U_n = np.random.normal(mu,sigma,n)
+
+xxn = xx + U_n
+
+plt.plot(tt, xxn, label ='Senoidal con Ruido y potencia normalizada')
+plt.grid()
+plt.legend()
+plt.show()
+#%%
+
+from scipy import signal as sig
+
+n0 = 300 # muestras
+dd = np.zeros(n0 + 1) # delta dirac --> creo un vector de ceros, y le digo que en un solo valor valga 1
+dd[n0] = 1 # Levanto el vector en n0 para que ahi valga 1
+
+plt.figure()
+plt.title(f'Delta en n0 = {n0}')
+plt.plot(dd)
+plt.grid()
+plt.show()
+
+yy = sig.convolve(xx, dd)
+
+plt.figure()
+plt.title(f'Senoidal corrida n0 = {n0} muestras')
+plt.plot(yy)
+plt.grid()
+plt.show()
+
+#%% CONVOLUCION
+# El tamaño es la longitud de una señal + la longitud de la otra señal - 1
+
+yy = 1/n*sig.convolve(U_n, np.flip(U_n))
+
+plt.figure()
+plt.title('Convolucion')
+plt.clf()
+plt.plot(yy)
+plt.grid()
+
+# La autocorrelacion siempre es par, tiene simetria par
+# La secuencia no es estrictamente incorrelada --> no es blanco --> aparece ese "piso" de ruido
+# La secuencia es idelmente incorrelada, por eso la delta no queda donde creemos que deberia
+# La transformada de Furier no va a ser una constante, porque no es una delta, es parecida a una delta 
+# --> me va a dar algo parecido a la transformada de una delta, el espectro me va a dar parecido a una constante
+
+#%% CUANTIZACION
+
+B = 4 # Cantidad de bits
+Vfs = 3 # [V] --> es el voltaje en full scale
+# --> asumo que es simetrico, en este caso como es 3 tengo 1.5 para arriba y 1.5 para abajo
+qq = Vfs / 2**B# Rango de cuantizacion --> qq = 0,1875
+
+# Para cuantizar tengo que truncar o redondear
+xxq = np.round(xx / qq) * qq # Esta cuantizada y codificada
+# Lo multiplico devuelta por qq para que me coincida con las unidades de xx
+
+plt.figure(figsize=(8,6))
+plt.subplot(2,1,1)
+plt.plot(xx, label='Señal original')
+plt.plot(xxq, label='Señal cuantizada')
+plt.grid(True)
+plt.legend()
+plt.title("Cuantización")
+
+# Calculo la secuencia de error
+# El error de cuntizacion queda acotado entre [-q/2; +q/2] --> en este caso [-0,1875; +0,1875]
+# Para calcular el error xx y xxq tienen que estar en la misma magnitud, en lass mismas unidades
+# --> Entonces multiplico devuelta por q, no se va a simplificar porque es una operacion no lineal
+e = xxq - xx # Es el error de cuantizacion
+
+# Grafico el error 
+plt.subplot(2,1,2)
+plt.plot(tt, e, label = 'Error de cuantizacion')
+plt.axhline( qq/2, color='red',   linewidth=0.8, linestyle='--', label=f'+q/2 = +{qq/2:.4f}V')
+plt.axhline(-qq/2, color='red',   linewidth=0.8, linestyle='--', label=f'-q/2 = -{qq/2:.4f}V')
+plt.legend()
+plt.grid(True)
+
+plt.xlabel("Muestras")
+plt.ylabel("Amplitud [V]")
+
+plt.tight_layout()
+plt.show()
+
+# --> En este caso tenemos algo correlado
+# Hay mucho ordenamiento temporal 
+
+#%% Ahora cuantizamos xxn que tiene un poco de ruido (20db de SNR)
+
+xxq = np.round(xxn / qq) * qq
+
+plt.figure()
+plt.subplot(2,1,1)
+plt.plot(xxn, label = 'Señal original con ruido')
+plt.plot(xxq, label = 'Señal cuantizada')
+plt.grid(True)
+plt.legend()
+plt.title("Cuantización con ruido")
+
+# Calculo la secuencia de error
+# El error de cuntizacion queda acotado entre [-q/2; +q/2] --> en este caso [-0,1875; +0,1875]
+# Para calcular el error xx y xxq tienen que estar en la misma magnitud, en lass mismas unidades
+# --> Entonces multiplico devuelta por q, no se va a simplificar porque es una operacion no lineal
+e = xxq - xxn # Es el error de cuantizacion
+
+# Grafico el error 
+plt.subplot(2,1,2)
+plt.plot(tt, e, label = 'Error de cuantizacion')
+plt.axhline( qq/2, color='red',   linewidth=0.8, linestyle='--', label=f'+q/2 = +{qq/2:.4f}V')
+plt.axhline(-qq/2, color='red',   linewidth=0.8, linestyle='--', label=f'-q/2 = -{qq/2:.4f}V')
+plt.legend()
+plt.grid(True)
+
+plt.xlabel("Muestras")
+plt.ylabel("Amplitud [V]")
+
+plt.tight_layout()
+plt.show()
+
+# --> En este caso tenemos algo fuertemente incorrelado, que tiene que ver con la potencia del ruido
+# Es decir que se "rompe" la correlacion --> eso es porque cambiamos Pr (la potencia), entonces hay mas energia, y hay exceso de ruido
+# --> Tiene que ver con la cantidad de bits
+# Cuando llego a los limites de cuantizacion, hace que se generen cambios de bits, cambios de niveles
+# --> Para romper con eso tiene que ser comparable con la mitad del paso de cuantizacion
+
+# La potencia del ruido es tan grande que hace que cuando lo queres representar se pasa al nivel siguiente de cuantizacion
+#%% SEÑAL CON RUIDO 
+
+# HISTOGRAMA 
+# --> es la funcion distribucion estocastica, es un estimador de la funcion distribucion de probabilidad
+# Es una aproximacion a la funcion teorica (cajita) 
+
+plt.figure(figsize=(10,6))
+
+plt.subplot(1,2,1)
+plt.hist(e, bins = 30, color = 'steelblue', density = True, edgecolor = 'dimgray', linewidth = 0.8) # La cantidad de bins me da mas rayitas, las cajitas son mas angostas. Si es muy angosta me va a caer en cero porque es muy angostita la cajita
+plt.axvline( qq/2, color='red',   linewidth=0.8, linestyle='--', label=f'+q/2 = +{qq/2:.4f}V')
+plt.axvline(-qq/2, color='red',   linewidth=0.8, linestyle='--', label=f'-q/2 = -{qq/2:.4f}V')
+plt.axhline(1/qq, color='darkorange',   linewidth=0.8, linestyle='--', label=f'1/q = {1/qq:.4f}V')
+plt.grid(True)
+plt.legend()
+plt.title("Histograma del error de cuantización (con ruido)")
+plt.xlabel("Error [V]")
+plt.ylabel("Densidad")
+
+# AUTOCORRELACION
+plt.subplot(1,2,2)
+
+r = np.correlate(e, e, mode = 'full')
+lags = np.arange(-len(e)+1, len(e))
+L = 50 # Cantidad de lags que quiero ver
+
+plt.plot(lags, r, label = 'Autocorrelacion', color = 'seagreen')
+plt.xlim(-L, L)
+plt.axvline(0, color='red',   linewidth=0.8, linestyle='--', label='Lag = 0')
+plt.legend()
+plt.grid(True)
+plt.title("Autocorrelación del error")
+plt.xlabel("Retardo (lag)")
+plt.ylabel("Autocorrelación")
+
+#%% SEÑAL SIN RUIDO 
+
+e = xxq - xx
+
+plt.figure(figsize=(10,6))
+
+plt.subplot(1,2,1)
+plt.hist(e, bins = 30, color = 'steelblue', density = True, edgecolor = 'dimgray', linewidth = 0.8) # La cantidad de bins me da mas rayitas, las cajitas son mas angostas. Si es muy angosta me va a caer en cero porque es muy angostita la cajita
+plt.axvline( qq/2, color='red',   linewidth=0.8, linestyle='--', label=f'+q/2 = +{qq/2:.4f}V')
+plt.axvline(-qq/2, color='red',   linewidth=0.8, linestyle='--', label=f'-q/2 = -{qq/2:.4f}V')
+plt.axhline(1/qq, color='darkorange',   linewidth=0.8, linestyle='--', label=f'1/q = {1/qq:.4f}V')
+plt.grid(True)
+plt.legend()
+plt.title("Histograma del error de cuantización (sin ruido)")
+plt.xlabel("Error [V]")
+plt.ylabel("Densidad")
+
+# AUTOCORRELACION
+plt.subplot(1,2,2)
+
+r = np.correlate(e, e, mode = 'full')
+lags = np.arange(-len(e)+1, len(e))
+L = 50 # Cantidad de lags que quiero ver
+
+plt.plot(lags, r, label = 'Autocorrelacion', color = 'seagreen')
+plt.xlim(-L, L)
+plt.axvline(0, color='red',   linewidth=0.8, linestyle='--', label='Lag = 0')
+plt.legend()
+plt.grid(True)
+plt.title("Autocorrelación del error")
+plt.xlabel("Retardo (lag)")
+plt.ylabel("Autocorrelación")
+
+
+
---
+++ Tareas semanales/TS1/TS1.py
@@ -0,0 +1,243 @@
+# Primero importo la libreria numpy
+import numpy as np
+import matplotlib.pyplot as plt
+import Funciones as ts1
+
+# %% ------------------------------- EJERCICIO 1 -------------------------------
+"""
+1) Sintetizar y graficar: --> uso funciones de numpy
+    a. Una señal sinusoidal de 2KHz
+    b. Misma señal amplificada y desfazada en pi/2
+    c. Señal anterior modulada en amplitud por otra señal sinusoidal de la mitad de frecuencia. --> modular es multiplicar por otra señal, en este caso va a ser de 1kHz --> sen(2*t) * sen(t) --> reemplazo A (amplitud) por la funcion que modula
+    d. Señal anterior recortada al 75% de su amplitud --> potencia = A**2 / 2 --> despues se hace con una funcion de numpy --> la salida es recta arriba y abajo --> recorto todo lo que supere el 75% de la potencia
+    --> las funciones con las que trabajamos no son periodicas (vemos N-muestras) --> vamos a calcular energias --> tomamos potencia como energia
+    --> tengo que recortar la amplitud al valor del 75% de la potencia de la señal
+    --> una manera es ir recorriendo el vector, y cuando llego a un valor que es mayor que el 75% de la potencia, lo cambio por el valor corte = treasure = 75% de la potencia
+    --> otra manera, busco la funcion de numpy --> np.clip
+    e. Una señal cuadrada de 4KHz
+    f. Un pulso rectangular de 10ms
+    g. En cada caso indique tiempo entre muestras, numero de mustras y potencia
+"""
+    
+#%% ITEM 1
+# ------------------------------- Señal sinusoidal de 2kHz -------------------------------
+# Defino mis variables 
+amplitud = 1
+offset = 0
+frecuencia = 2000 # 2kHz = 2000 Hz
+fase = 0
+N = 100 # Cantidad de muestras
+fs = 20000 # Pongo fs > 2 * frecuencia --> Teorema de Nyquist-Shannon 
+
+# Llamo a mi funcion
+_,f1 = ts1.mi_funcion_sen(amplitud, offset, frecuencia, fase, N, fs)
+
+# Genero otra ventana para los graficos
+plt.figure()  # Tamaño de la figura (ancho, alto)
+plt.subplot(2,2,1)
+
+plt.title("Señal Sinusoidal")
+
+# Grafico la señal senoidal de 2KHz
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud [V]')
+plt.plot(_,f1, 'o--',  label = 'Sinusoidal de 2kHz')
+plt.legend()
+plt.show()
+
+# TIEMPO ENTRE MUESTRAS
+Ts = 1/fs
+print(f"El tiempo entre muestras es {Ts} segundos")
+
+# Calculo la potenacia
+x = ts1.calcular_potencia(f1)
+print("Potencia de la señal f1: ", x)
+
+#%% ITEM 2
+# ------------------------------- Señal sinusoidal de 2kHz, amplificada y desfazada en pi/2 -------------------------------
+# Defino mis variables 
+amplitud = 2
+offset = 0
+frecuencia = 2000 # 2kHz = 2000 Hz
+fase = np.pi/2
+N = 100 # Cantidad de muestras
+fs = 20000
+
+# Llamo a mi funcion
+_,f2 = ts1.mi_funcion_sen(amplitud, offset, frecuencia, fase, N, fs) 
+
+# Grafico la señal senoidal de 2KHz, pero amplificada y desfazada en pi/2
+plt.subplot(2,2,2)
+plt.title("Señal Sinusoidal")
+
+# Grafico la señal senoidal de 2KHz
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud [V]')
+plt.plot(_,f1, 'o--',  label = 'Sinusoidal de 2kHz')
+plt.legend()
+
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud [V]')
+# plt.plot(_,f2, 'x:', label = f'2kHz, amplificada y desfazada en {fase}')
+plt.plot(_,f2, 'x:', label = '2kHz, amplificada y desfazada en π/2')
+plt.legend()
+plt.grid()
+plt.show()
+
+# TIEMPO ENTRE MUESTRAS
+Ts = 1/fs
+print(f"El tiempo entre muestras es {Ts} segundos")
+
+# Calculo la potenacia
+x = ts1.calcular_potencia(f2)
+print("Potencia de la señal f2: ", x)
+
+#%% ITEM 3
+# ------------------------------- Señal sinusoidal modulada por otra señal sinusoidal de 1kHz -------------------------------
+# Defino mis variables
+amplitud = 1
+offset = 0
+frecuencia = 2000 # 2kHz = 2000 Hz
+fase = 0
+N = 100 # Cantidad de muestras
+fs = 20000
+
+# Llamo a mi funcion
+_,f3 = ts1.mi_funcion_sen_modulada(amplitud, offset, frecuencia, fase, N, fs)
+
+# Grafico la señal senoidal de 2KHz, pero modulada por otra señal de la mitad de la frecuencia
+plt.subplot(2,2,3)
+plt.title("Señal Sinusoidal")
+
+# Grafico la señal senoidal de 2KHz
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud [V]')
+plt.plot(_,f1, 'o--',  label = 'Sinusoidal de 2kHz')
+plt.legend()
+
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud [V]')
+plt.plot(_, f3, 'x:', label = 'Señal modulada') # Genero el grafico de la señal
+plt.legend()
+plt.grid()
+plt.show()
+
+# TIEMPO ENTRE MUESTRAS
+Ts = 1/fs
+print(f"El tiempo entre muestras es {Ts} segundos")
+
+# Calculo la potenacia
+x = ts1.calcular_potencia(f3)
+print("Potencia de la señal f3: ", x)
+
+#%% ITEM 4
+# ------------------------------- Misma señal pero recortada al 75% de su potencia -------------------------------
+# Defino mis variables
+amplitud = 1
+offset = 0
+frecuencia = 2000 # 2kHz = 2000 Hz
+fase = 0
+N = 100 # Cantidad de muestras
+fs = 20000
+
+# Llamo a mi funcion
+_,f4 = ts1.mi_funcion_sen_recortada(amplitud, offset, frecuencia, fase, N, fs)
+
+plt.subplot(2,2,4)
+plt.title("Señal Sinusoidal")
+
+# Grafico la señal senoidal de 2KHz
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud [V]')
+plt.plot(_,f1, 'o--',  label = 'Sinusoidal de 2kHz')
+plt.legend()
+
+# Grafico la señal recortada
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud [V]')
+plt.plot(_,f4, '*--', label = 'Señal recortada al 75%')
+plt.legend()
+plt.grid()
+plt.show()
+
+# TIEMPO ENTRE MUESTRAS
+Ts = 1/fs
+print(f"El tiempo entre muestras es {Ts} segundos")
+
+# Calculo la potenacia
+x = ts1.calcular_potencia(f4)
+print("Potencia de la señal f4: ", x)
+
+#%% ITEM 5
+# ------------------------------- Señal cuadrada de 4kHz -------------------------------
+# Defino mis variables
+frecuencia = 4000
+fs = 20000
+N = 50 # Cantidad de muestras
+offset = 0
+fase = 0
+
+# Llamo a mi funcion
+_,f5 = ts1.mi_funcion_cuadrada(frecuencia, fs, N, offset, fase) # Comparo dos cosas de valores muy diferentes
+
+# Grafico la señal cuadrada
+plt.figure()
+plt.subplot(1,2,1)
+plt.title('Señal cuadrada de 4kHz')
+plt.xlabel('Tiempo [s]')
+plt.ylabel('Amplitud [V]')
+plt.plot(_,f5, 'o--', label = 'Señal cuadrada de 4Hz')
+plt.legend()
+plt.grid()
+plt.show()
+
+# TIEMPO ENTRE MUESTRAS
+Ts = 1/fs
+print(f"El tiempo entre muestras es {Ts} segundos")
+
+# Calculo la potenacia
+x = ts1.calcular_potencia(f5)
+print("Potencia de la señal f5: ", x)
+
+#%% ITEM 6
+# ------------------------------- Pulso rectangular de 10ms -------------------------------
+# Defino mis variables
+t0 = 1 # Tiempo donde empieza el pulso
+tf = 11 # Tiempo donde termina el pulso 
+# --> Con este t0 y tf mi pulso rectangular dura 10ms
+N = 100 # Cantidad de muestras
+h = 1 # Altura del pulso 
+
+# Llamo a mi funcion
+f6 = ts1.mi_funcion_pulso(t0, tf, N, h)
+
+# Grafico
+plt.subplot(1,2,2)
+plt.title('Pulso rectangular de 10ms')
+plt.plot(f6, 'o--', label = 'Pulso rectangular de 10ms')
+# otra manera de graficar el pulso: 
+# plt.stem(x)
+plt.xlabel('Tiempo[ms]')
+plt.ylabel('Amplitud [V]')
+plt.axis([-2,15,0,1.5]) #Limites del grafico ([Xmin,Xmax,Ymin,Ymax]), si no se agrega este plt (axis) el programa por default busca mostrar todo el grafico completo
+plt.legend()
+plt.grid()
+plt.show()
+
+# TIEMPO ENTRE MUESTRAS
+Ts = 1/fs
+print(f"El tiempo entre muestras es {Ts} segundos")
+
+# Calculo la energia
+x = ts1.calcular_energia(f6)
+print("Energia de la señal f6: ", x)
+
+#%% OBSERVACIONES
+# Pongo N = 100, porque si pongo N = 1000 el tiempo entre muestras es demasiado chico y no se aprecian las señales en los graficos
+# De todas formas, con el N seleccionado tambien el Ts es muy bajo pero no tanto
+# --> Esta bien que sea bajo, porque las señales que estamos graficando tienen frecuencias muy altas
+# --> Como cumplo Nyquist para evitar aliasing, cuando hago Ts = fs/2, se hace muy chico ese tiempo
+
+
+
+
