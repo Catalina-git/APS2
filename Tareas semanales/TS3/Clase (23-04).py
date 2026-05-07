da34f63d96b1224653ee52b7d50fb23b0122b536
Catalina <catagonzalezaraujo@gmail.com>
2026-05-07 12:56:13 -0300

    TS3

--- Tareas semanales/TS3/Clase (23-04).py
+++ Tareas semanales/TS3/Clase (23-04).py
@@ -319,74 +319,46 @@ plt.show()
 # A cada una de las senoidales le pusimos amplitud = raiz de 2 --> eso hace que la potencia este normalizada, es decir que sea unitaria.
 # Entonces verifico la identidad de Parseval en cada caso 
 
-print("Identidad de Parseval\n")
+print("\nIdentidad de Parseval\n")
 
-#%% FRECUENCIA N/4 
-vmax = np.sqrt(2)
-dc = 0
-x = dc + vmax * np.sin(2*np.pi*f0*tt)
+#%% frecuencia = N/4
 
-# Calculo la varianza
-varianza_x = np.var(x)
+# Potencia en tiempo
+P_tiempo = np.mean(x**2)
 
-# Caluclo la FFT
-X = np.fft.fft(x) / N
+# Potencia en frecuencia
+P_freq = np.sum(np.abs(X/N)**2)
 
-modulo_X = np.abs(X)**2
-# Parseval --> ambas energias tienen que ser iguales
-E_t = np.sum(np.abs(x)**2) # Energia en el tiempo
-E_f = (1/N) * np.sum(modulo_X) # Energia en frecuencia
-diferencia = np.abs(E_t - E_f) # Tiene que dar cero, o muy cercano a cero
+print("----- Senoidal con frecuencia = N/4 -----")
+print(f"Potencia temporal: {P_tiempo:.4f}")
+print(f"Potencia frecuencial: {P_freq:.4f}")
 
-print("------ Senoidal con frecuencia = N/4 ------")
-print(f"Varianza = {varianza_x:.3f}")
-print("Parseval...")
-print(f"Enetgía en tiempo: {E_t:.3f}")
-print(f"Enetgía en frecuencia: {E_f:.3f}")
-print(f"Diferencia: {diferencia:.3f}")
+#%% frecuencia = N/4 + 0.25
 
-#%% FRECUENCIA N/4 + 0.5 
-# Calculo la varianza
-varianza_x1 = np.var(x1)
+P_tiempo1 = np.mean(x1**2)
+P_freq1 = np.sum(np.abs(X1/N)**2)
 
-# Caluclo la FFT
-X1 = np.fft.fft(x1) / N
+print("----- Senoidal con frecuencia = N/4 + 0.25 -----")
+print(f"Potencia temporal: {P_tiempo1:.4f}")
+print(f"Potencia frecuencial: {P_freq1:.4f}")
 
-modulo_X1 = np.abs(X1)**2
-# Parseval --> ambas energias tienen que ser iguales
-E_t1 = np.sum(np.abs(x1)**2) # Energia en el tiempo
-E_f1 = (1/N) * np.sum(modulo_X1) # Energia en frecuencia
-diferencia1 = np.abs(E_t1 - E_f1) # Tiene que dar cero, o muy cercano a cero
+#%% frecuencia = N/4 + 0.5
 
-print("\n------ Senoidal con frecuencia = N/4 + 0.5 ------")
-print(f"Varianza = {varianza_x1:.3f}")
-print("Parseval...")
-print(f"Enetgía en tiempo: {E_t1:.3f}")
-print(f"Enetgía en frecuencia: {E_f1:.3f}")
-print(f"Diferencia: {diferencia1:.3f}")
+P_tiempo2 = np.mean(x2**2)
+P_freq2 = np.sum(np.abs(X2/N)**2)
 
-#%% FRECUENCIA N/4 + 1
-# Calculo la varianza
-varianza_x2 = np.var(x2)
+print("----- Senoidal con frecuencia = N/4 + 0.5 -----")
+print(f"Potencia temporal: {P_tiempo2:.4f}")
+print(f"Potencia frecuencial: {P_freq2:.4f}")
 
-# Caluclo la FFT
-X2 = np.fft.fft(x2) / N
+#%% frecuencia = N/4 + 1
 
-modulo_X2 = np.abs(X2)**2
-# Parseval --> ambas energias tienen que ser iguales
-E_t2 = np.sum(np.abs(x2)**2) # Energia en el tiempo
-E_f2 = (1/N) * np.sum(modulo_X2) # Energia en frecuencia
-diferencia2 = np.abs(E_t2 - E_f2) # Tiene que dar cero, o muy cercano a cero
+P_tiempo3 = np.mean(x3**2)
+P_freq3 = np.sum(np.abs(X3/N)**2)
 
-print("\n------ Senoidal con frecuencia = N/4 + 1 ------")
-print(f"Varianza = {varianza_x2:.3f}")
-print("Parseval...")
-print(f"Enetgía en tiempo: {E_t2:.3f}")
-print(f"Enetgía en frecuencia: {E_f2:.3f}")
-print(f"Diferencia: {diferencia2:.3f}")
-
-
-
+print("----- Senoidal con frecuencia = N/4 + 1 -----")
+print(f"Potencia temporal: {P_tiempo3:.4f}")
+print(f"Potencia frecuencial: {P_freq3:.4f}")
 
 
 
