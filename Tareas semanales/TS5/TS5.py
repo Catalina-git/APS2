dcf83084528a03b5e307c4a63f0f6b9da898e591
Catalina Gonzalez Araujo <catagonzalezaraujo@gmail.com>
2026-05-21 21:35:03 -0300

    TS5

--- Tareas semanales/TS5/TS5.py
+++ Tareas semanales/TS5/TS5.py
@@ -16,6 +16,7 @@ from scipy.io.wavfile import write
 # average = 'mean'
 
 #%% ECG SIN RUIDO
+# Pasa bajos
 
 ecg_one_lead = np.load('ecg_sin_ruido.npy')
 N_ECG = len(ecg_one_lead)
@@ -26,6 +27,7 @@ nn_ECG = np.arange(N_ECG)
 plt.figure()
 plt.plot(nn_ECG, ecg_one_lead)
 plt.title("ECG sin ruido")
+plt.show()
 
 fs_ecg = 1000 # Hz
 cant_promedio = 10
@@ -35,10 +37,21 @@ f, Pxx = sig.welch(ecg_one_lead, fs = fs_ecg, window ='hamming', nperseg = npers
 
 plt.figure()
 plt.title("WELCH - ECG")
-plt.plot(f, Pxx)
+# plt.plot(f, Pxx)
 plt.xlim(-1, 45)
 
+
+cant_promedio = 30
+nperseg = ecg_one_lead.shape[0] // cant_promedio
+
+f, Pxx = sig.welch(ecg_one_lead, fs = fs_ecg, window ='hamming', nperseg = nperseg, noverlap = 15)
+
+plt.plot(f, Pxx)
+
+plt.show()
+
 #%% PPG SIN RUIDO
+# Pasa bajos 
 
 ppg = np.load('ppg_sin_ruido.npy')
 
@@ -56,6 +69,8 @@ plt.plot(f_ppg, Pxx_ppg)
 plt.xlim(-1, 45)
 
 #%% AUDIO 
+# Pasa banda
+
 # LA CUCARACHA
 fs_audio1, wav_data1 = sio.wavfile.read('la cucaracha.wav')
 
@@ -66,9 +81,17 @@ plt.plot(wav_data1)
 cant_promedio_sonido = 100 
 nperseg = wav_data1.shape[0] // cant_promedio_sonido
 
-f_audio1, Pxx_audio1 = sig.welch(wav_data1, fs = fs_audio1, window ='hamming', nperseg = nperseg)
+f_audio1, Pxx_audio1 = sig.welch(wav_data1, fs = fs_audio1, window ='hamming', nperseg = nperseg, noverlap = 50)
 
 plt.figure()
 plt.title("WELCH - AUDIO")
 plt.plot(f_audio1, Pxx_audio1)
 plt.xlim(-1, 2500)
+
+#%% Para estimar el ancho de banda
+# Hago la potencia acumulada a izquierda, y cuando llego al final del ancho de banda tengo que haber acumulado el total de la potencia
+# Acumulo el 95% de la potencia (ahi tengo el ancho de banda) --> de toda mi potencia, 5% que queda es ruido 
+# Pero si la se;al no tiene ruido? En la practica decir que no tiene ruido es decir que no tiene ruido considerable 
+# Por eso hablo del 5%, 3%, 2% de la potencia 
+# Entonces hablo de un contenido espectral en cuanto a la potencia bajisimo 
+
